192d60f604442889e500e1bd2fd59b62e92e0eca
manasaa2811 <75196743+manasaa2811@users.noreply.github.com>
2020-12-05 20:43:00 +0530

    Search-App

---
+++ NMN Apparel Store/Search-App/backend/data.js
@@ -0,0 +1,124 @@
+export default {
+    products: [
+        {
+          _id:'1',
+          name:'Shirt',
+          category: 'Men',
+          image:'/images/mshirt.jpg',
+          price:500,
+          brand:'RR'
+      },
+      {
+          _id:'2',
+          name:'Tshirt',
+          category: 'Men',
+          image:'/images/tshirt.jpg',
+          price:899,
+          brand:'Polo'
+      },
+      {
+          _id:'3',
+          name:'jacket',
+          category:'Men',
+          image:'/images/jacket.jpg',
+          price:399,
+          brand:'TR'
+      },
+      {
+          _id:'4',
+          name:'Watch',
+          category: 'Men',
+          image:'/images/watch.jpg',
+          price:999,
+          brand:'Gucci'
+      },
+      {
+          _id:'5',
+          name:'Wallet',
+          category: 'Men',
+          image:'/images/wallet.jpg',
+          price:699,
+          brand:'HM'
+      },
+      {
+          _id:'6',
+          name:'Belt',
+          category: 'Men',
+          image:'/images/belt.jpg',
+          price:465,
+          brand:'WC'
+      },
+      {
+          _id:'7',
+          name:'Floral dress',
+          category: 'Women',
+          image:'/images/clothes.jpg',
+          price:500,
+          brand:'Soch'
+      },
+      {
+          _id:'8',
+          name:'Maxi',
+          category: 'Women',
+          image:'/images/maxi.jpg',
+          price:500,
+          brand:'Soch'
+      },
+      {
+          _id:'9',
+          name:'Converse',
+          category: 'Men',
+          image:'/images/converse.jpg',
+          price:500,
+          brand:'Soch'
+      },
+      {
+        _id:'10',
+        name:'Frock',
+        category: 'Kids',
+        image:'/images/kid1.jpg',
+        price:200,
+        brand:'SK'
+    },
+    {
+        _id:'11',
+        name:'Dress',
+        category: 'Kids',
+        image:'/images/kid2.jpg',
+        price:340,
+        brand:'SK'
+    },
+    {
+        _id:'12',
+        name:'Shoe',
+        category: 'Kids',
+        image:'/images/kid3.jpg',
+        price:299,
+        brand:'SS'
+    },
+    {
+        _id:'13',
+        name:'Hairband',
+        category: 'Kids',
+        image:'/images/kid4.jpg',
+        price:599,
+        brand:'SK'
+    },
+    {
+        _id:'14',
+        name:'Bag',
+        category: 'Women',
+        image:'/images/bag.jpg',
+        price:1500,
+        brand:'Gucci'
+    },
+    {
+        _id:'15',
+        name:'Heels',
+        category: 'Women',
+        image:'/images/heels.jpg',
+        price:780,
+        brand:'TT'
+    },
+    ]
+    }
