bcdcdaff4e9764e92e09df2df4a6aff261064f86
Nidhi Gupta <56179878+NidhxGupta@users.noreply.github.com>
2020-12-07 10:16:53 +0530

    Updated User Dashboard

--- NMN Apparel Store/Frontend/user-app/src/containers/HomePage/index.js
+++ NMN Apparel Store/Frontend/user-app/src/containers/HomePage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Jumbotron } from "react-bootstrap";
+import { Col, Container, Jumbotron, Card, Button } from "react-bootstrap";
 import { isUserLoggedIn } from "../../actions";
 import Layout from "../../components/Layout";
 import { Carousel, Row } from "react-bootstrap";
@@ -7,6 +7,15 @@ import "./style.css";
 import { NavLink } from "react-router-dom";
 import mensOffer_image from "./images/Men.png";
 import womensOffer_image from "./images/Women.png";
+import jockey_men from "./images/Jockey_Men.png"
+import addidas_men from "./images/Addidas_Pants.png"
+import jockey_tracks from "./images/Jockey_WTracks.png"
+import soch_saree from "./images/Soch_WSaree.png"
+import fasttrack_watch from "./images/Fasttrack_WWatch.png"
+import kids_jumpsuit from "./images/TASLAR_KJumpsuitFlannel.png"
+import kids_ethnic from "./images/Soch_KEthnicSet.png"
+import kids_winter from "./images/TRENDS_KWinterWear.png"
+import puma_men from "./images/Puma_Jacket.png"
 import kidsOffer_image from "./images/Kids.png";
 import carousel from "./images/Carousel.png";
 import Footer from "../../components/Footer";
@@ -67,60 +76,148 @@ const HomePage = (props) => {
                     <Row>
                         <Container>
                             <br></br>
-                            <p class="text-center"><h1>MEN</h1></p>
+                            <p class="text-center">
+                                <h1>MEN</h1>
+                            </p>
                         </Container>
                     </Row>
                     <Row>
-                        <Container fluid>
-                            <Col>
-                                {/*Add Mens Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Mens Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Mens Product Card here*/}
-                            </Col>
-                        </Container>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={jockey_men} />
+                                <Card.Body>
+                                    <Card.Title>Jockey</Card.Title>
+                                    <Card.Text>
+                                        <p>Men's T-Shirt</p>
+                                        <p><b>₹1000</b></p>
+                                    </Card.Text>
+                                    <Button variant="primary">Buy Now</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={addidas_men} />
+                                <Card.Body>
+                                    <Card.Title>Addidas</Card.Title>
+                                    <Card.Text>
+                                        <p>Men's Running Tracks</p>
+                                        <p><b>₹700</b></p>
+                                    </Card.Text>
+                                    <Button variant="primary">Buy Now</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={puma_men} />
+                                <Card.Body>
+                                    <Card.Title>Puma</Card.Title>
+                                    <Card.Text>
+                                        <p>Men's Jacket</p>
+                                        <p><b>₹700</b></p>
+                                    </Card.Text>
+                                    <Button variant="primary">Buy Now</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     </Row>
                     <Row>
                         <Container>
                             <br></br>
-                            <p class="text-center"><h1>WOMEN</h1></p>
+                            <p class="text-center">
+                                <h1>WOMEN</h1>
+                            </p>
                         </Container>
                     </Row>
                     <Row>
-                        <Container fluid>
-                            <Col>
-                                {/*Add Womens Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Womens Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Womens Product Card here*/}
-                            </Col>
-                        </Container>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={jockey_tracks} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.
+                  </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={soch_saree} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={fasttrack_watch} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     </Row>
                     <Row>
                         <Container>
                             <br></br>
-                            <p class="text-center"><h1>KIDS</h1></p>
+                            <p class="text-center">
+                                <h1>KIDS</h1>
+                            </p>
                         </Container>
                     </Row>
                     <Row>
-                        <Container fluid>
-                            <Col>
-                                {/*Add Kids Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Kids Product Card here*/}
-                            </Col>
-                            <Col>
-                                {/*Add Kids Product Card here*/}
-                            </Col>
-                        </Container>
+
+                        <Col><Card style={{ width: "18rem" }}>
+                            <Card.Img variant="top" src={kids_winter} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make
+                                    up the bulk of the card's content.
+                                    </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card></Col>
+                        <Col><Card style={{ width: "18rem" }}>
+                            <Card.Img variant="top" src={kids_jumpsuit} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make
+                                    up the bulk of the card's content.
+                                    </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card></Col>
+                        <Col><Card style={{ width: "18rem" }}>
+                            <Card.Img variant="top" src={kids_ethnic} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make
+                                    up the bulk of the card's content.
+                                    </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card></Col>
+
                     </Row>
+                    <Row><br></br>
+                        <br></br></Row>
                     <br></br>
                     <br></br>
                 </Col>
@@ -131,3 +228,4 @@ const HomePage = (props) => {
 };
 
 export default HomePage;
+
